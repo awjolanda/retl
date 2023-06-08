c0008c45571e510a2e5fe38388b912208fef6158
Gregory Reiter <greg1166@outlook.com>
2023-06-08 15:35:52 +0200

    front-end

--- front_end/retl/src/App.js
+++ front_end/retl/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef } from 'react';
+import Naver from './components/Naver.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.js';
+import List_all from './pages/List_all.js';
+import Emperor from './pages/Emperor.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Naver/>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route exact path="/list_all" element={<List_all/>} />
+                <Route exact path="/emperor" element={<Emperor/>} />
+            </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ front_end/retl/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SPQR from '../ASSest/SPQR_sign.png';
+import styles from './home.module.css';
+import { Link } from 'react-router-dom';
+
+
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <h1>VENI VIDI VICI</h1>
+            <Link to="/home"><img className={styles.image} src={SPQR}/></Link>
+            <h1>MMXXIII</h1>
+        </div>
+    )
+}
+
+export default Home;
